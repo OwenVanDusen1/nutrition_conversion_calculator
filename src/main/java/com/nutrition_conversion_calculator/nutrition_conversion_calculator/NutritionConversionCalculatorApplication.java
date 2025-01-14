b5141fd5f9acad2114b874dd48f2d0cb8669b643
Owen VanDusen <owenvandusen@cmail.carleton.ca>
2025-01-14 16:52:33 -0500

    Created Spring Boot App Skeleton

---
+++ src/main/java/com/nutrition_conversion_calculator/nutrition_conversion_calculator/NutritionConversionCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.nutrition_conversion_calculator.nutrition_conversion_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutritionConversionCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutritionConversionCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nutrition_conversion_calculator/nutrition_conversion_calculator/NutritionConversionCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutrition_conversion_calculator.nutrition_conversion_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutritionConversionCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
