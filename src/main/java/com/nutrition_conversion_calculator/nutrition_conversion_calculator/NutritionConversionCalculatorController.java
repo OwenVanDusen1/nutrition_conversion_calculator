bc3f3a743292ddbeac67ab42a8f9d48498dbe66d
Owen VanDusen <owenvandusen@cmail.carleton.ca>
2025-02-05 16:38:39 -0500

    Added autocomplete text box for selecting food - preset list currently, needs to talk to database later

--- src/main/java/com/nutrition_conversion_calculator/nutrition_conversion_calculator/NutritionConversionCalculatorController.java
+++ src/main/java/com/nutrition_conversion_calculator/nutrition_conversion_calculator/NutritionConversionCalculatorController.java
@@ -1,12 +1,44 @@
 package com.nutrition_conversion_calculator.nutrition_conversion_calculator;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+import java.util.List;
+import java.util.ArrayList;
+
+
+@Controller
 public class NutritionConversionCalculatorController {
 
     @Autowired
     FoodRepository foodRepository;
+
+    /**
+     * 
+     * @return
+     */
+    @GetMapping("/view_calculator")
+    public String viewCalculator() {
+        return "view_calculator";
+    }
+
+    /**
+     * 
+     */
+    @RequestMapping(value="/autofill")
+    @ResponseBody
+    public List<String> autofill(@RequestParam(value="term", required = false, defaultValue = "") String term) {
+        List<String> suggestions = new ArrayList<String>();
+        // Add options here for manual testing
+        suggestions.add("Hello");
+        suggestions.add("Help find cars");
+        suggestions.add("Hospitality");
+        return suggestions;
+
+    }
 
 }
